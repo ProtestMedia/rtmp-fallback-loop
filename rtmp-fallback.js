7523a0980b657ea4d6f016215e52dbfb2621150f
sebseb7 <sebgreenbus@gmail.com>
2020-10-12 23:54:49 +0000

    genesis

---
+++ rtmp-fallback.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+const spawn = require("child_process").spawn;
+
+const input_rtmp = 'rtmp://127.0.0.1/feed/1';
+const loop_file  	 = 'loop.mp4';
+const out_rtmp   = 'rtmp://127.0.0.1/feed/3';
+const timeout    = 10; // in 1/10th of a second
+
+
+var fallback=0;
+var mode = 'initial';
+
+function initInputStream(){
+
+	let toReturn = spawn("ffmpeg", ("-f live_flv -re -i "+input_rtmp+" -c copy -f mpegts -").split(" "));
+	toReturn.on("exit", (code) => { console.log("ffmpegIn exited with code " + code); ffmpegIn = initInputStream() });
+	toReturn.stdout.on("data", onData);
+
+	return toReturn;
+}
+
+function initLoopStream(){
+
+	let toReturn = spawn("ffmpeg", ("-stream_loop -1 -re -i "+loop_file+" -c copy -f mpegts -").split(" "));
+	toReturn.on("exit", (code) => { console.log("ffmpegIn exited with code " + code); ffmpegLoop = initLoopStream() });
+	toReturn.stdout.on("data", onDataLoop);
+
+	return toReturn;
+}
+
+const ffmpegOut = spawn("ffmpeg", ("-fflags +genpts -re -f mpegts -i - -c copy -bsf:a aac_adtstoasc -f flv "+out_rtmp).split(" "));
+//ffmpegOut.stderr.on("data", (data) => { console.log(data.toString())});
+//ffmpegOut.stdout.on("data", (data) => { console.log(data.toString())});
+ffmpegOut.on("exit", (code) => { console.log("ffmpegOut exited with code "+code+"! Exiting..."); process.exit(code); });
+
+var ffmpegIn = initInputStream();
+var ffmpegLoop = initLoopStream();
+
+function onData(videoData) {
+
+	if(mode != 'input') {
+		console.log('send input');
+		mode = 'input';
+	}
+	ffmpegOut.stdin.write(videoData);
+	fallback=0;
+}
+
+function onDataLoop(videoData) {
+	
+	if(mode != 'initial')
+	if(fallback > timeout){
+		if(mode == 'input') {
+			console.log('send loop');
+			mode = 'loop';
+		}
+		ffmpegOut.stdin.write(videoData);
+	}
+}
+
+setInterval(function(){
+	fallback++;
+}, 100);
+
+
